5db2e64f57951b8e22e2fe79e428def7d61f1721
peageruk <98ducanhdo@gmail.com>
2023-05-17 03:31:37 +0700

    condition

--- src/components/AddNewProduct.js
+++ src/components/AddNewProduct.js
@@ -6,61 +6,90 @@ const AddNewProduct = () => {
   const [size, setSize] = useState(0);
   const [color, setColor] = useState("");
   const handleOnClickBtn = () => {
-    console.log(">>>check input", name, price, size, color);
+    let object = {
+      name,
+      price,
+      size,
+      color,
+    };
+    console.log(">>>check input", object);
   };
+  const [isShow, setItShow] = useState("true");
   return (
-    <fieldset>
-      <legend>AddNewProduct</legend>
-      <div className="input-product">
-        <label> Name: </label>
-        <input
-          value={name}
-          type="text"
-          onChange={(event) => {
-            setName(event.target.value);
-          }}
-        />
-      </div>
-      <div className="input-product">
-        <label> Price: </label>
-        <input
-          value={price}
-          type="text"
-          onChange={(event) => {
-            setPrice(event.target.value);
-          }}
-        />
-      </div>
-      <div className="input-product">
-        <label> Size: </label>
-        <input
-          value={size}
-          type="text"
-          onChange={(event) => {
-            setSize(event.target.value);
-          }}
-        />
-      </div>
-      <div className="input-product">
-        <label> Color: </label>
-        <input
-          value={color}
-          type="text"
-          onChange={(event) => {
-            setColor(event.target.value);
-          }}
-        />
-      </div>
-      <div>
-        <button
+    <div>
+      {isShow ? (
+        <>
+          <fieldset>
+            <legend>AddNewProduct</legend>
+            <div className="input-product">
+              <label> Name: </label>
+              <input
+                value={name}
+                type="text"
+                onChange={(event) => {
+                  setName(event.target.value);
+                }}
+              />
+            </div>
+            <div className="input-product">
+              <label> Price: </label>
+              <input
+                value={price}
+                type="text"
+                onChange={(event) => {
+                  setPrice(event.target.value);
+                }}
+              />
+            </div>
+            <div className="input-product">
+              <label> Size: </label>
+              <input
+                value={size}
+                type="text"
+                onChange={(event) => {
+                  setSize(event.target.value);
+                }}
+              />
+            </div>
+            <div className="input-product">
+              <label> Color: </label>
+              <input
+                value={color}
+                type="text"
+                onChange={(event) => {
+                  setColor(event.target.value);
+                }}
+              />
+            </div>
+            <div>
+              <button
+                onClick={() => {
+                  handleOnClickBtn();
+                }}
+              >
+                Add new
+              </button>
+            </div>
+          </fieldset>
+
+          <div
+            onClick={() => {
+              setItShow(!isShow);
+            }}
+          >
+            Hide this form
+          </div>
+        </>
+      ) : (
+        <div
           onClick={() => {
-            handleOnClickBtn();
+            setItShow(!isShow);
           }}
         >
-          Add new
-        </button>
-      </div>
-    </fieldset>
+          Show form
+        </div>
+      )}
+    </div>
   );
 };
 
