d58ba76aca4f358957e3dee00e5aa22a4ce8de69
peageruk <98ducanhdo@gmail.com>
2023-05-17 00:44:51 +0700

    validate input todo

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,24 +2,24 @@ import React, { useState } from "react";
 import _ from "lodash";
 const TodoList = () => {
   const [todo, setToDo] = useState("");
-  const [toDoList, setToDoList] = useState([
+  const [listToDo, setToDoList] = useState([
     { id: "todo1", todo: "test" },
     { id: "todo2", todo: "run" },
     { id: "todo3", todo: "debug" },
   ]);
   const handleOnClickBtn = () => {
     let toDo = { id: `todo3${getRndInteger(4, 999)}`, todo: todo };
-    let newToDO = [...toDoList, toDo];
-    setToDoList(newToDO);
-    console.log(">>> on click", todo);
+    let newToDO = [...listToDo, toDo];
+    !todo ? alert("missing todo") : setToDoList(newToDO);
+    setToDo("");
   };
   const getRndInteger = (min, max) => {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   };
   const onClickDelete = (id) => {
-    let newToDoList = _.clone(toDoList);
-    newToDoList = newToDoList.filter((item) => item.id !== id);
-    setToDoList(newToDoList);
+    let newListToDo = _.clone(listToDo);
+    newListToDo = newListToDo.filter((item) => item.id !== id);
+    setToDoList(newListToDo);
   };
   return (
     <div>
@@ -29,7 +29,6 @@ const TodoList = () => {
         type="text"
         onChange={(event) => {
           setToDo(event.target.value);
-          //   console.log(">>> check event : ", event);
         }}
       />
       <button
@@ -40,7 +39,7 @@ const TodoList = () => {
         add
       </button>
       <div>list to do:</div>
-      {toDoList.map((item) => {
+      {listToDo.map((item) => {
         return (
           <div
             key={item.id}
